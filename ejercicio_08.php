1d499f3157d5c2e42de93f6dbfc4014ae39b86d1
CristianRs11 <crodriguescalvo@gmail.com>
2022-01-21 09:17:15 +0100

    Exercicio 8

---
+++ ejercicio_08.php
@@ -0,0 +1,12 @@
+<html>
+	<body>
+		<?php
+            $euro = 100;
+            $conversor = 166;
+            $peseta = $euro * $conversor;
+			echo "
+            <p>$euro euros son $peseta pesetas.</p>
+            "
+		?>
+	</body>
+</html>
