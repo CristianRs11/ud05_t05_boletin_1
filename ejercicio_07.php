85e032b53d3e237c32c9e11c31aa82cc2db6e196
CristianRs11 <crodriguescalvo@gmail.com>
2022-01-21 09:14:38 +0100

    Exercicio 7

---
+++ ejercicio_07.php
@@ -0,0 +1,14 @@
+<html>
+	<body>
+		<?php
+            $nombre = "Cristian Rodrigues Calvo";
+            $direccion = "Calle Ave del Paraíso Nº13";
+            $telefono = "678901234";
+			echo "
+            <h1>$nombre</h1>
+            <h2>$direccion</h2>
+            <h3>$telefono</h3>
+            "
+		?>
+	</body>
+</html>
