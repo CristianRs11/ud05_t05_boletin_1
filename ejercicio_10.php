62bba433f1188c3d8008e11741e306d22486dafb
CristianRs11 <crodriguescalvo@gmail.com>
2022-01-21 09:33:09 +0100

    Exercicio 10

---
+++ ejercicio_10.php
@@ -0,0 +1,12 @@
+<html>
+	<body>
+		<?php
+            for ($i=1; $i <=9 ; $i++) { 
+                for ($m=0; $m <= $i-1; $m++) { 
+                    echo "*";
+                };
+                echo "<br/>";
+            }
+		?>
+	</body>
+</html>
